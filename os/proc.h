cb047767779b6e7f842fff45f94c8b966b74c639
adhiraj-chanana <143457403+adhiraj-chanana@users.noreply.github.com>
2026-02-17 23:47:39 +0000

    chapter 3

--- os/proc.h
+++ os/proc.h
@@ -4,6 +4,13 @@
 #include "types.h"
 
 #define NPROC (16)
+#define MAX_SYSCALL_NUM (500)
+
+typedef enum {
+    TaskStatusRunning = 2,
+} TaskStatus;
+
+
 
 // Saved registers for kernel context switches.
 struct context {
@@ -38,11 +45,18 @@ struct proc {
 	/*
 	* LAB1: you may need to add some new fields here
 	*/
+	uint64 start_time;
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
 };
 
 /*
 * LAB1: you may need to define struct for TaskInfo here
 */
+struct TaskInfo {
+    TaskStatus status;
+    unsigned int syscall_times[MAX_SYSCALL_NUM];
+    int time;
+};
 
 struct proc *curr_proc();
 void exit(int);
--- os/syscall.c
+++ os/syscall.c
@@ -39,6 +39,21 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz)
 /*
 * LAB1: you may need to define sys_task_info here
 */
+int sys_task_info(struct TaskInfo *ti)
+{
+    struct proc *p = curr_proc();
+    
+    ti->status = TaskStatusRunning;
+    
+    for (int i = 0; i < MAX_SYSCALL_NUM; i++) {
+        ti->syscall_times[i] = p->syscall_times[i];
+    }
+    
+    uint64 current_time = get_cycle() / (CPU_FREQ / 1000);
+    ti->time = (int)(current_time - p->start_time);
+    
+    return 0;
+}
 
 extern char trap_page[];
 
@@ -53,6 +68,9 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	if (id >= 0 && id < MAX_SYSCALL_NUM) {
+    curr_proc()->syscall_times[id]++;
+}
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -69,6 +87,9 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+	case SYS_task_info:
+    ret = sys_task_info((struct TaskInfo *)args[0]);
+    break;
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
